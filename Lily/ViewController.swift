261cd6ddd2d5df4e1ed2cf996d62094a43027bbb
oalejel <alejel.omar@gmail.com>
2017-10-28 10:40:04 -0400

    Basic visual navigation with theme set

--- Lily/ViewController.swift
+++ Lily/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        (parent as? UINavigationController)?.navigationBar.prefersLargeTitles = true
+        (parent as? UINavigationController)?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        
+        let flowerImage = UIImage(named: "lily_flower")!.withRenderingMode(.alwaysOriginal)
+//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+//        button.contentMode = .scaleAspectFit
+//        button.setImage(flowerImage, for: .normal)
+//        let iconItem = UIBarButtonItem(customView: button)
+        let flowerItem = UIBarButtonItem(image: flowerImage, style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = flowerItem
     }
 
     override func didReceiveMemoryWarning() {
